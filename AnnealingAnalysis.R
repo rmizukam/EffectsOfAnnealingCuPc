5c67a1cc1eb739788a2d2361e39ea74a7de8cdb5
Ryan Mizukami <rmizukami741@gmail.com>
2022-07-28 05:59:57 -0700

    readability

--- AnnealingAnalysis.R
+++ AnnealingAnalysis.R
@@ -1,22 +1,32 @@
 library(annealingDataAFMRM)
 library(dplyr)
+library(nanoscopeAFM)
 library(devtools)
 
 catalog <- dataRAW
 
+# Scans of RM20220701GL02A
+# ==============================================================================
 plot(RM20220701GL02A[[8]])
 plot(RM20220701GL02A[[9]])
 plot(RM20220701GL02A[[10]])
 plot(RM20220701GL02A[[11]])
 plot(RM20220701GL02A[[12]])
 
+
+
+# Scans of RM20220701GL02B
+# ==============================================================================
 plot(RM20220701GL02B[[13]])
 plot(RM20220701GL02B[[14]])
 plot(RM20220701GL02B[[15]])
 
+
+
+# Scans of RM20220701GL04B
+# ==============================================================================
 plot(RM20220701GL04B[[16]])
 plot(RM20220701GL04B[[17]])
 plot(RM20220701GL04B[[18]])
 
-AFMinfo(RM20220701GL02A[[12]])
 
